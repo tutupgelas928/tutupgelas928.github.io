3829a3b78b57eba0d71f2b14e809baacf1e55d00
nengmaya538 <nengmaya538@gmail.com>
2026-07-31 11:37:16 +0700

    Update robots.txt.js

--- functions/robots.txt.js
+++ functions/robots.txt.js
@@ -12,7 +12,7 @@ Sitemap: https://${host}/sitemap.xml
   return new Response(robots, {
     headers: {
       "Content-Type": "text/plain; charset=UTF-8",
-      "Cache-Control": "public, max-age=3600"
+      "Cache-Control": "public, max-age=5184000"
     }
   });
 }
