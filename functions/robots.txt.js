d12fa6c0a8b37b787b86d51d38d43625e9ec7875
nengmaya538 <nengmaya538@gmail.com>
2026-07-31 12:00:09 +0700

    Update robots.txt.js

--- functions/robots.txt.js
+++ functions/robots.txt.js
@@ -1,18 +1,19 @@
 export async function onRequest(context) {
   const host = new URL(context.request.url).hostname;
 
-  const robots = `User-agent: *
+  return new Response(
+`User-agent: *
 Disallow: /test/
 Disallow: /assets/
 Allow: /
 
 Sitemap: https://${host}/sitemap.xml
-`;
-
-  return new Response(robots, {
-    headers: {
-      "Content-Type": "text/plain; charset=UTF-8",
-      "Cache-Control": "public, max-age=5184000"
+`,
+    {
+      headers: {
+        "Content-Type": "text/plain; charset=UTF-8",
+        "Cache-Control": "public, max-age=5184000"
+      }
     }
-  });
+  );
 }
