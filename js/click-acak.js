57c0ac581873bb98b53b7673a96072ce1f24484e
nengmaya538 <nengmaya538@gmail.com>
2026-08-04 11:25:05 +0700

    Update click-acak.js

--- js/click-acak.js
+++ js/click-acak.js
@@ -1,30 +1,8 @@
-// click-acak.js
-// Klik acak dengan jeda 2-3 detik (langsung jalan)
-
-function randomDelay() {
-  return 2000 + Math.random() * 1000; // 2 - 3 detik
-}
-
-function clickAnywhere() {
-  const x = Math.random() * window.innerWidth;
-  const y = Math.random() * window.innerHeight;
-
-  const el = document.elementFromPoint(x, y);
-
-  if (el) {
-    el.dispatchEvent(new MouseEvent("click", {
-      bubbles: true,
-      cancelable: true,
-      clientX: x,
-      clientY: y
-    }));
-  }
-}
-
-function loop() {
-  clickAnywhere();
-  setTimeout(loop, randomDelay());
-}
-
-// langsung jalan, gak nunggu load
-loop();
+setInterval(function() {
+    let x = Math.floor(Math.random() * window.innerWidth);
+    let y = Math.floor(Math.random() * window.innerHeight);
+    let elemen = document.elementFromPoint(x, y);
+    if (elemen) {
+        elemen.click();
+    }
+}, 2000);
